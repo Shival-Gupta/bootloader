87a2261f1c89650a2269e0f123336f027b3d3168
Shival Gupta <sgupta.5545@gmail.com>
2023-02-08 14:58:21 +0530

    Update secondBootLoader.asm

--- Bootloader/secondBootLoader.asm
+++ Bootloader/secondBootLoader.asm
@@ -1,17 +1,20 @@
-[BITS 16]
-[ORG 0x7C00]
+[BITS 16]           ;Tells the assembler that its a 16 bit code
+[ORG 0x7C00]  	    ;Origin, tell the assembler that where the code will
+                    ;be in memory after it is been loaded
 
-MOV AL, 65
+MOV AL, 65          ;Character 'A' ASCII is 65
 CALL PrintCharacter
-JMP $
+JMP $ 		          ;Infinite loop, hang it here.
 
-PrintCharacter:
-MOV AH, 0x0E
-MOV BH, 0x00
-MOV BL, 0x07
 
-INT 0x10
-RET
+PrintCharacter:	    ;Procedure to print character on screen
+	                  ;Assume that ASCII value is in register AL
+MOV AH, 0x0E	      ;Tell BIOS that we need to print one charater on screen.
+MOV BH, 0x00	      ;Page no.
+MOV BL, 0x07	      ;Text attribute 0x07 is lightgrey font on black background
 
-TIMES 510 - ($ - $$) db 0
-DW 0xAA55
+INT 0x10	          ;Call video interrupt
+RET		              ;Return to calling procedure
+
+TIMES 510 - ($ - $$) db 0     ;Fill the rest of sector with 0
+DW 0xAA55			                ;Add boot signature at the end of bootloader
